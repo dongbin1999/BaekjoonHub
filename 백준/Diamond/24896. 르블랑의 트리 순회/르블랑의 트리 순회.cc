003d395aa151d87697cdd4b97899c32b42fc65e4
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-05-06 14:15:29 +0900

    [Diamond V] Title: 르블랑의 트리 순회, Time: 896 ms, Memory: 85800 KB -BaekjoonHub

--- 백준/Diamond/24896. 르블랑의 트리 순회/르블랑의 트리 순회.cc
+++ 백준/Diamond/24896. 르블랑의 트리 순회/르블랑의 트리 순회.cc
@@ -1,46 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<set<int>> tree;
-vector<bool> leaf;
+vector<vector<int>> tree,tmp;
+vector<bool> cut;
+queue<int> q;
 int n;
 void dfs(int node,int pa){
-    leaf[node]=tree[node].size()==1;
-    if(tree[node].size()==2){
-        auto it=tree[node].begin();
-        int x=*it; it++;int y=*it;
-        tree[x].erase(find(tree[x].begin(),tree[x].end(),node));
-        tree[y].erase(find(tree[y].begin(),tree[y].end(),node));
-        tree[x].insert(y);
-        tree[y].insert(x);
-        tree[node].clear();
-        x==pa?dfs(y,x):dfs(x,y);
-        return;
-    }
-    set<int> tmp=tree[node];
-    for(auto nx:tmp)
+    if(tree[node].size()==1)cut[node]=1,q.push(node);
+    for(auto nx:tree[node])
         if(nx!=pa)dfs(nx,node);
 }
+void dfs2(int node,int pa){
+    if(tree[node].size()!=2||cut[node])return;
+    cut[node]=1;
+    for(auto nx:tree[node])
+        if(nx!=pa)dfs2(nx,node);
+}
 
-void refactoring(){
-    leaf.assign(n+1,0);
-    for(int i=1;i<=n;i++)if(tree[i].size()==1){dfs(i,i);break;}
+void cutting(){
+    cut.assign(n+1,0);
+    tmp.assign(n+1,{});
+    for(int i=1;i<=n;i++)if(!tree[i].empty()){dfs(i,i);break;}
+    while(!q.empty())
+        dfs2(tree[q.front()][0],q.front()),q.pop();
     for(int i=1;i<=n;i++)
-        if(leaf[i]){
-            int x=*tree[i].begin();
-            auto it=find(tree[x].begin(),tree[x].end(),i);
-            if(it!=tree[x].end())tree[x].erase(it);
-            tree[i].clear();
-        }
+        for(auto j:tree[i])
+            if(!cut[i]&&!cut[j])tmp[i].push_back(j);
+    tree=tmp;
 }
 int main(){
     scanf("%d",&n);
     tree.assign(n+1,{});
     for(int i=1;i<n;i++){
         int a,b;scanf("%d%d",&a,&b);
-        tree[a].insert(b);
-        tree[b].insert(a);
+        tree[a].push_back(b);
+        tree[b].push_back(a);
     }
-    int m=2;while(m--)refactoring();
+    int m=2;while(m--)cutting();
     for(auto v:tree)if(v.size()>=3)return !printf("NO");
     return !printf("YES");
 }
