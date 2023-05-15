565b28e0231a9a1fc6f3e4d73fe7c658189a0784
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-05-15 13:37:25 +0900

    [Platinum V] Title: XOR Pairs, Time: 20 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Platinum/26850. XOR Pairs/XOR Pairs.cc
+++ 백준/Platinum/26850. XOR Pairs/XOR Pairs.cc
@@ -1,20 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define f(x) (1<<(31-__builtin_clz(x)))
-typedef long long ll;
+#include<cstdio>
+bool v[1<<20];
 int main(){
-    int n,m;scanf("%d%d",&n,&m);
-    vector<bool> v(1000001,0);
-    while(m--){
-        int x;scanf("%d",&x);
-        v[x]=1;
-    }
-    ll ans=0;
-    for(int i=3;i<=n;i++){
-        if(v[i])continue;
-        int x=i-f(i);
-        while(x)ans+=f(x),x-=f(x);
-    }
+    int n,m,x;scanf("%d%d",&n,&m);
+    while(m--)scanf("%d",&x),v[x]=1;
+    long long ans=0;
+    for(int i=3;i<=n;i++)ans+=v[i]?0:i-(1<<(31-__builtin_clz(i)));
     printf("%lld",ans<<1);
-    return 0;
 }
