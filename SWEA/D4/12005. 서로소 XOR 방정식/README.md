b1d07196fdf2277e2a40c24cafb185d0d8345111
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-06-01 23:28:27 +0900

    [D4] Title: 서로소 XOR 방정식, Time: 209 ms, Memory: 13,544 KB -BaekjoonHub

---
+++ SWEA/D4/12005. 서로소 XOR 방정식/README.md
@@ -0,0 +1,11 @@
+# [D4] 서로소 XOR 방정식 - 12005 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXkcTJ5q8qEDFAS8) 
+
+### 성능 요약
+
+메모리: 13,544 KB, 시간: 209 ms, 코드길이: 625 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/12005. 서로소 XOR 방정식/서로소 XOR 방정식.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll sz=1000;
+ll GCD(ll a,ll b){return b?GCD(b,a%b):a;}
+
+ll f(ll n,ll m){
+    ll ret=0;
+    for(ll i=n+1;i<=n+sz;i++){
+        if(GCD(i,n)==1ll)ret++;
+        if(ret==m) return i;
+    }
+    return 3e18;
+}
+
+void solve(){
+    ll k,m;scanf("%lld %lld",&k,&m);
+    ll ans=3e18;
+    for(ll XOR=0;XOR<=sz;XOR++){
+        ll n=XOR^k;
+        if(f(n,m)-n==XOR)ans=min(ans,n);
+    }
+    printf("%lld\n",ans>2e18?-1ll:ans);
+}
+
+int main(){
+    int tc;scanf("%d",&tc);
+    for(int TC=1;TC<=tc;TC++){
+        printf("#%d ",TC);
+        solve();
+    }
+    return 0;
+}
