7733757042304e42c1e1411eb13399df24eac7fa
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-06-22 01:02:11 +0900

    [Platinum III] Title: functionx, Time: 364 ms, Memory: 26600 KB -BaekjoonHub

--- 백준/Platinum/25317. functionx/functionx.cc
+++ 백준/Platinum/25317. functionx/functionx.cc
@@ -3,32 +3,15 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> pll;
 const int sz=1<<18;
-ll pl[sz*2],mi[sz*2],pllazy[sz*2],milazy[sz*2];
+ll pl[sz*2],mi[sz*2];
 
-void propagate(int node,int ns,int ne,int c){
-    if(!(c?pllazy:milazy)[node]) return;
-    if(node<sz){
-        (c?pllazy:milazy)[node*2]+=(c?pllazy:milazy)[node];
-        (c?pllazy:milazy)[node*2+1]+=(c?pllazy:milazy)[node];
-    }
-    (c?pl:mi)[node]+=(c?pllazy:milazy)[node]*(ll)(ne-ns+1);
-    (c?pllazy:milazy)[node]=0LL;
+bool cmp(pll a,pll b){__int128 aa=a.first,bb=a.second,cc=b.first,dd=b.second;return aa*dd<bb*cc;}
+
+void update(int i,int c){
+    i+=sz,(c?pl:mi)[i]++;
+    while(i>1)i/=2,(c?pl:mi)[i]=(c?pl:mi)[i*2]+(c?pl:mi)[i*2+1];
 }
-
-void update(int s,int e,ll k,int node,int ns,int ne,int c){
-    propagate(node,ns,ne,c);
-    if(e<ns||ne<s)return;
-    if (s<=ns&&ne<=e){
-        (c?pllazy:milazy)[node]+=k;
-        propagate(node,ns,ne,c); return;
-    }
-    int mid=(ns+ne)/2;
-    update(s,e,k,node*2,ns,mid,c),update(s,e,k,node*2+1,mid+1,ne,c);
-    (c?pl:mi)[node]=(c?pl:mi)[node*2]+(c?pl:mi)[node*2+1];
-}
-
 ll query(int s,int e,int node,int ns,int ne,int c){
-    propagate(node,ns,ne,c);
     if(e<ns||ne<s)return 0;
     if(s<=ns&&ne<=e)return (c?pl:mi)[node];
     int mid=(ns+ne)/2;
@@ -53,10 +36,7 @@ int main(){
             p.push_back({a,1ll});
         }
     }
-    sort(p.begin(),p.end(),[](pll a,pll b){
-        __int128 aa=a.first,bb=a.second,cc=b.first,dd=b.second;
-        return aa*dd<bb*cc;
-    });
+    sort(p.begin(),p.end(),cmp);
     bool END=0,rev=0;
     for(auto qu:queries){
         ll c=qu[0],a=qu[1],b=qu[2];
@@ -66,23 +46,14 @@ int main(){
             else if(a!=0){
                 pll x={-b,a};
                 if(a<0)x.first*=-1ll,x.second*=-1ll;
-                ll l=lower_bound(p.begin(),p.end(),x,[](pll a,pll b){
-                    __int128 aa=a.first,bb=a.second,cc=b.first,dd=b.second;
-                    return aa*dd<bb*cc;
-                })-p.begin();
-                update(l,l,1ll,1,0,sz-1,a>0);
+                int l=lower_bound(p.begin(),p.end(),x,cmp)-p.begin();
+                update(l,a>0);
             }
         }
         else{
             pll x={-b,a};
-            ll l=lower_bound(p.begin(),p.end(),x,[](pll a,pll b){
-                __int128 aa=a.first,bb=a.second,cc=b.first,dd=b.second;
-                return aa*dd<bb*cc;
-            })-p.begin(),
-                    r=upper_bound(p.begin(),p.end(),x,[](pll a,pll b){
-                __int128 aa=a.first,bb=a.second,cc=b.first,dd=b.second;
-                return aa*dd<bb*cc;
-            })-p.begin()-1;
+            int l=lower_bound(p.begin(),p.end(),x,cmp)-p.begin(),
+            r=upper_bound(p.begin(),p.end(),x,cmp)-p.begin()-1;
             if(END||query(l,r,1,0,sz-1,0)||query(l,r,1,0,sz-1,1))
                 printf("0\n");
             else {
