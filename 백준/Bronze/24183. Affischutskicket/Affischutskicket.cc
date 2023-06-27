8252b3c18f33a2cee492cd07cef23676da7a41b4
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-06-27 23:17:53 +0900

    [Bronze IV] Title: Affischutskicket, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24183. Affischutskicket/Affischutskicket.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a,b,c;scanf("%lf%lf%lf",&a,&b,&c);
+    printf("%lf",a*0.229*0.324*2.0+b*0.297*0.42*2.0+c*0.21*0.297);
+    return 0;
+}
