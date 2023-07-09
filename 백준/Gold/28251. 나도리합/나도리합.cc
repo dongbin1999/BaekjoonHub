acf14185c3cad14710b16662e08ac26e6b0664d2
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-07-09 13:34:55 +0900

    [Gold III] Title: 나도리합, Time: 164 ms, Memory: 5144 KB -BaekjoonHub

--- 백준/Gold/28251. 나도리합/나도리합.cc
+++ 백준/Gold/28251. 나도리합/나도리합.cc
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll u[200001],sum[200001],ans[200001];
-vector<ll> v[200001];
+ll u[200001],ans[200001];
 int find(int a){
-    if(u[a]<0)return a;
+    if(u[a]<=0)return a;
     return u[a]=find(u[a]);
 }
 ll merge(int a,int b){
     a=find(a),b=find(b);
     if(a==b)return ans[a];
-    if(v[a]<v[b])swap(a,b);
-    ans[a]+=ans[b],u[a]+=u[b];
-    while(!v[b].empty())
-        ans[a]+=v[b].back()*sum[a],v[a].push_back(v[b].back()),v[b].pop_back();
-    sum[a]+=sum[b],u[b]=a;
+    ans[a]+=u[a]*u[b]+ans[b],u[a]+=u[b],u[b]=a;
     return ans[a];
 }
 int main(){
     int n,q;scanf("%d%d",&n,&q);
     for(int i=1;i<=n;i++){
         ll x;scanf("%lld",&x);
-        v[i].push_back(x),u[i]--,sum[i]=x;
+        u[i]-=x;
     }
     while(q--){
         int a,b;scanf("%d%d",&a,&b);
