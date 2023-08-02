81adbe7165ff923d81e3d6bc4dd535b9025309fe
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-08-02 20:05:51 +0900

    [Gold IV] Title: 세 수, 두 M, Time: 32 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/2405. 세 수， 두 M/세 수， 두 M.cc
+++ 백준/Gold/2405. 세 수， 두 M/세 수， 두 M.cc
@@ -7,8 +7,7 @@ int main(){
     for(int i=0;i<n;i++)scanf("%d",&v[i]);
     sort(v.begin(),v.end());
     int ans=0;
-    for(int i=1;i<n-1;i++)ans=max({ans,abs(v[0]+v[i+1]-2*v[i]),abs(v[i-1]+v.back()-2*v[i]),
-                                   abs(v[0]+v.back()-2*v[i]),abs(v[i-1]+v[i+1]-2*v[i])});
+    for(int i=1;i<n-1;i++)ans=max({ans,abs(v[0]+v[i+1]-2*v[i]),abs(v[i-1]+v.back()-2*v[i]),});
     printf("%d",ans);
     return 0;
 }
