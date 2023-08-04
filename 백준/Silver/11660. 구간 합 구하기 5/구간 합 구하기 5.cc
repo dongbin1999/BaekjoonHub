306d10ea4f34d5dee65d3aa0a75e13f28ca3d090
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-08-04 14:25:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 204 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,27 +1,16 @@
-#include <cstdio>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pii pair<int,int>
-const int sz = (1 << 10);
+int a[1025][1025],sum[1025][1025];
 
-int a[sz + 1][sz + 1], sum[sz + 1][sz + 1];
-
-int main()
-{
-    int n, m;
-    scanf("%d %d", &n, &m);
-    for (int x = 1; x <= n; x++)
-        for (int y = 1; y <= n; y++)
-        {
-            scanf("%d", &a[x][y]);
-            sum[x][y] = sum[x - 1][y] + sum[x][y - 1] - sum[x - 1][y - 1] + a[x][y];
-        }
-
-    int x, y, x2, y2;
-    while (m--)
-    {
-        scanf("%d %d %d %d", &x, &y, &x2, &y2);
-        printf("%d\n", sum[x2][y2] - sum[x2][y - 1] - sum[x - 1][y2] + sum[x - 1][y - 1]);
+int main(){
+    int n,m;scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;i++)for(int j=1;j<=n;j++){
+        scanf("%d",&a[i][j]);
+        sum[i][j]=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+a[i][j];
     }
+    while(m--){
+        int x1,y1,x2,y2;scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+        printf("%d\n",sum[x2][y2]-sum[x1-1][y2]-sum[x2][y1-1]+sum[x1-1][y1-1]);
+    }
+    return 0;
 }
