0e30f4ed72b6e6613bf74324ae5f01d5cc4a61b5
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-08-10 01:30:48 +0900

    [Diamond V] Title: 돌 옮기기, Time: 8 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Diamond/28436. 돌 옮기기/돌 옮기기.cc
+++ 백준/Diamond/28436. 돌 옮기기/돌 옮기기.cc
@@ -1,21 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
-char s[500002];
+char s[1<<19];
 void solve(){
-    scanf("%s",s);int n=strlen(s);
-    ll ans=0,bw=0;
-    for(int i=n-1;i>=0;i--){
-        if(s[i]=='B')bw=min(0ll,bw-1);
-        else if(s[i]=='W')bw=max(0ll,bw+1);
-        else ans+=bw;
-    }
-    printf(ans>0?"WHITE\n":"BLACK\n");
+    scanf("%s",s+1);
+    long long a=0,x=0;
+    for(int i=strlen(s+1);i;i--)
+        if(s[i]=='B')x=min(0ll,x-1);
+        else if(s[i]=='W')x=max(0ll,x+1);
+        else a+=x;
+    printf(a>0?"WHITE\n":"BLACK\n");
 }
-
 int main(){
-    int tc;scanf("%d",&tc);
-    while(tc--)solve();
-    return 0;
+    int t;scanf("%d",&t);
+    while(t--)solve();
 }
