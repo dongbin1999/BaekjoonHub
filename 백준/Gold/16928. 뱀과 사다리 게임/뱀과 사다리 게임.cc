26fcd6e911cdeb0085db91185a8f567b08d4dce3
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-08-09 17:42:44 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -7,15 +7,15 @@ int main(){
     n+=m;
     while(n--){
         int x,y;scanf("%d%d",&x,&y);
-        if(x>=1&&x<=100)a[x]=y;
+        a[x]=y;
     }
     queue<pii> q;vis[1]=1,q.push({1,0});
     while(!q.empty()){
         auto [x,turn]=q.front();q.pop();
         if(x==100)return !printf("%d",turn);
         for(int i=x+1;i<=min(100,x+6);i++){
-            int y=i;while(a[y]>=1&&a[y]<=100)y=a[y];
-            if(y>=1&&y<=100&&!vis[y])vis[y]=1,q.push({y,turn+1});
+            int y=i;while(a[y])y=a[y];
+            if(!vis[y])vis[y]=1,q.push({y,turn+1});
         }
     }
     return 0;
