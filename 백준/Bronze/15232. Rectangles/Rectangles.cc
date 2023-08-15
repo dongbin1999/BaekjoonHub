5de5f2cc74e86982ee5d86ff15614eb52264575c
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-08-15 21:35:42 +0900

    [Bronze IV] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int r,c;scanf("%d%d",&r,&c);
+    while(r--){
+        int x=c;while(x--)printf("*");
+        printf("\n");
+    }
+    return 0;
+}
