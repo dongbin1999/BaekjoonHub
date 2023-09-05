9666edbffa47fafa5f08318f22db831457bbd436
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2023-09-05 16:43:39 +0900

    [Gold I] Title: 구간 성분, Time: 172 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/10840. 구간 성분/구간 성분.cc
+++ 백준/Gold/10840. 구간 성분/구간 성분.cc
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int sum[1501][26][2];
-ll base=1501,mod[2]={1000000007,1000000009};
-ll getHash(vector<int> v){
-    ll ret[2]={0};
+const int base=1501;
+const ll mod[2]={1000000007,1000000009};
+ll power[26][2],psum[1500][2][2];
+char a[1503],b[1503];
+
+void init(char *s,bool c){
+    int n=strlen(s);
     for(int p=0;p<2;p++)
-        for(int i=0;i<26;i++)
-            ret[p]=(ret[p]*base+v[i])%mod[p];
+        for(int i=0;i<n;i++)
+            psum[i][p][c]=i?(psum[i-1][p][c]+power[s[i]-'a'][p])%mod[p]:power[s[0]-'a'][p];
+}
+ll getHash(int s,int e,bool c){
+    ll ret[2]; if(s>e)return -1;
+    for(int p=0;p<2;p++)
+        ret[p]=(psum[e][p][c]-(s?psum[s-1][p][c]:0)+mod[p])%mod[p];
     return ret[0]*mod[1]+ret[1];
 }
 
 int main(){
-    string a,b;cin>>a>>b;
-    for(int i=0;i<a.size();i++)
-        for(int j=0;j<26;j++)
-            sum[i][j][0]=(a[i]-'a'==j)+(i?sum[i-1][j][0]:0);
-    for(int i=0;i<b.size();i++)
-        for(int j=0;j<26;j++)
-            sum[i][j][1]=(b[i]-'a'==j)+(i?sum[i-1][j][1]:0);
-    map<ll,int> ma;
-    for(int l=0;l<a.size();l++)for(int r=l;r<a.size();r++){
-            vector<int> v(26);
-            for(int i=0;i<26;i++)v[i]=sum[r][i][0]-(l?sum[l-1][i][0]:0);
-            ma[getHash(v)]=r-l+1;
-        }
-    int ans=0;
-    for(int l=0;l<b.size();l++)for(int r=l;r<b.size();r++){
-            vector<int> v(26);
-            for(int i=0;i<26;i++)v[i]=sum[r][i][1]-(l?sum[l-1][i][1]:0);
-            ans=max(ans,ma[getHash(v)]);
-        }
-    printf("%d",ans);
+    for(int i=0;i<26;i++)for(int p=0;p<2;p++)
+        power[i][p]=i?power[i-1][p]*base%mod[p]:1;
+    scanf("%s %s",a,b);
+    int n=strlen(a),m=strlen(b);
+    init(a,0),init(b,1);
+    for(int len=min(n,m);len>=1;len--){
+        set<ll> se;
+        for(int l=0,r=len-1;r<n;l++,r++)se.insert(getHash(l,r,0));
+        for(int l=0,r=len-1;r<m;l++,r++)if(se.count(getHash(l,r,1)))return !printf("%d",len);
+    }
+    printf("0");
     return 0;
 }
