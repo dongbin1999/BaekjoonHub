524e602c0f90ea219069ce7ad05fb28f44147d7a
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-01-07 19:05:35 +0900

    [level unrated] Title: 산 모양 타일링, Time: 0.94 ms, Memory: 5.81 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod=10007;
+//정삼각형 블록 4개를 위,오른쪽순으로 1,2,3,4번으로 가정.
+int dp[100001][2];//1/0 : 2번블록이 이미 채워진거/안채워진거.근데 채우려면 보라색 블록으로만 채워야함.
+
+int solution(int n, vector<int> tops) {
+    memset(dp,0,sizeof(dp));
+    dp[0][0]=1;
+    for(int i=0;i<n;i++){
+        if(tops[i]){
+            dp[i+1][0]=(dp[i+1][0]+dp[i][0]*3)%mod;//123
+            dp[i+1][1]=(dp[i+1][1]+dp[i][0]*1)%mod;//1234
+            dp[i+1][0]=(dp[i+1][0]+dp[i][1]*2)%mod;//13
+            dp[i+1][1]=(dp[i+1][1]+dp[i][1]*1)%mod;//134
+        }
+        else{
+            dp[i+1][0]=(dp[i+1][0]+dp[i][0]*2)%mod;//23
+            dp[i+1][1]=(dp[i+1][1]+dp[i][0]*1)%mod;//234
+            dp[i+1][0]=(dp[i+1][0]+dp[i][1]*1)%mod;//3
+            dp[i+1][1]=(dp[i+1][1]+dp[i][1]*1)%mod;//34
+        }
+        //printf("%d : %d %d\n",i+1,dp[i+1][0],dp[i+1][1]);
+    }
+    return (dp[n][0]+dp[n][1])%mod;
+}
