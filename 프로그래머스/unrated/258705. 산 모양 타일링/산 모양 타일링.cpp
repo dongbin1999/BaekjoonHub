59817d310b646d60cdaed5a8f8e1b21403c530cf
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-01-09 01:26:33 +0900

    [level unrated] Title: 산 모양 타일링, Time: 1.34 ms, Memory: 5.78 MB -BaekjoonHub

--- 프로그래머스/unrated/258705. 산 모양 타일링/산 모양 타일링.cpp
+++ 프로그래머스/unrated/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -1,7 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int mod=10007;
-//정삼각형 블록 4개를 위,오른쪽순으로 1,2,3,4번으로 가정.
+/*
+정삼각형 블록 4개를 위,오른쪽순으로 1,2,3,4번으로 가정.
+문제의 두 번째 그림을 예시로 들면,
+첫 번째 도형 : 1, 두 번째 도형 : 23, 세 번째 도형 : 34, 네 번째 도형 : 13.
+*/
 int dp[100001][2];//1/0 : 2번블록이 이미 채워진거/안채워진거.근데 채우려면 보라색 블록으로만 채워야함.
 
 int solution(int n, vector<int> tops) {
@@ -20,7 +24,6 @@ int solution(int n, vector<int> tops) {
             dp[i+1][0]=(dp[i+1][0]+dp[i][1]*1)%mod;//3
             dp[i+1][1]=(dp[i+1][1]+dp[i][1]*1)%mod;//34
         }
-        //printf("%d : %d %d\n",i+1,dp[i+1][0],dp[i+1][1]);
     }
     return (dp[n][0]+dp[n][1])%mod;
 }
