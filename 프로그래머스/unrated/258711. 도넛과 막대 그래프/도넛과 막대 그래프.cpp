a6f7c79f066c4575906a2bded755f1967945166f
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-01-09 01:08:31 +0900

    [level unrated] Title: 도넛과 막대 그래프, Time: 104.03 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/unrated/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/unrated/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -1,11 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int u[1000001];
-int find(int a){
-    if(u[a]<0)return a;
-    else return u[a]=find(u[a]);
-}
+int u[1000001],in[1000001],out[1000001],exist[1000001];
+
+//union-find
+int find(int a){return u[a]<0?a:u[a]=find(u[a]);}
 bool merge(int a,int b){
     a=find(a),b=find(b);
     if(a==b)return false;
@@ -15,17 +14,24 @@ bool merge(int a,int b){
 
 vector<int> solution(vector<vector<int>> edges) {
     int add=-1,doughnut=0,stick=0,eight=0;
-    vector<int> in(1000001),out(1000001),vis(1000001);
+
+    for(auto i:edges)in[i[1]]++,out[i[0]]++,exist[i[1]]=exist[i[0]]=1;
     
-    for(auto i:edges)in[i[1]]++,out[i[0]]++,vis[i[1]]=vis[i[0]]=1;
+    //생성한 정점 찾기. (특징 : 나가는 간선 2개 이상, 들어오는 간선 없음)
     for(int i=1;i<=1000000;i++)if(out[i]>=2&&!in[i]){add=i;break;}
     
+    //생성한 정점과 직접 이어진 간선들 모두 제거.
     for(auto i:edges)if(i[0]==add||i[1]==add)in[i[1]]--,out[i[0]]--;
+    exist[add]=0;
+    
+    //막대 모양 그래프 특징 : 나가는 간선이 없는 정점이 유일하게 존재.
+    //8자 모양 그래프 특징 : 나가는 간선, 들어오는 간선이 각각 정확히 2개인 정점이 유일하게 존재.
     for(int i=1;i<=1000000;i++){
-        if(vis[i]&&i!=add&&!out[i])stick++;
-        if(out[i]==in[i]&&out[i]==2)eight++;
+        if(exist[i]&&!out[i])stick++;
+        if(in[i]==2&&out[i]==2)eight++;
     }
     
+    //그래프 연결 후,
     memset(u,-1,sizeof(u));
     for(auto i:edges){
         int s=i[0],e=i[1];
@@ -34,7 +40,10 @@ vector<int> solution(vector<vector<int>> edges) {
     }
     
     int all=0;
-    for(int i=1;i<=1000000;i++)all+=vis[i]&&i==find(i)&&i!=add;
+    //그래프 덩어리 개수 세기.
+    for(int i=1;i<=1000000;i++)all+=exist[i]&&i==find(i);
+    
+    //남은 덩어리는 도넛 모양 그래프의 개수.
     doughnut=all-stick-eight;
     return {add,doughnut,stick,eight};
 }
