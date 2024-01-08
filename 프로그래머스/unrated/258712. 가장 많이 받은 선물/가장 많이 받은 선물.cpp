dedcdc37514e36a8e1f1f24a0f8518caa89b62bb
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-01-09 00:57:00 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 3.08 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -4,22 +4,20 @@ using namespace std;
 int solution(vector<string> friends, vector<string> gifts) {
     map<string,map<string,int>> ma;
     map<string,int> gift;
+    string give,get;
+    
     for(auto i:gifts){
-        string a,b;
-        int idx=0;
-        while(i[idx]!=' ')idx++;
-        for(int j=0;j<idx;j++)a+=i[j];
-        for(int j=idx+1;j<i.size();j++)b+=i[j];
-        ma[a][b]++,ma[b][a]--;
-        gift[a]++,gift[b]--;
+        istringstream iss(i);
+        iss>>give>>get;
+        ma[give][get]++,ma[get][give]--;
+        gift[give]++,gift[get]--;
     }
-    vector<int> ans(friends.size());
-    for(int i=0;i<friends.size();i++)
-        for(int j=0;j<friends.size();j++){
-            if(i==j)continue;
-            string a=friends[i],b=friends[j];
-            if(ma[a][b]>0||(ma[a][b]==0&&gift[a]>gift[b]))
-                ans[i]++;
-        }
-    return *max_element(ans.begin(),ans.end());
+    
+    int answer=0;
+    map<string,int> ans;
+    for(auto a:friends)for(auto b:friends)
+        if(ma[a][b]>0||(ma[a][b]==0&&gift[a]>gift[b]))
+                ans[a]++,answer=max(answer,ans[a]);
+
+    return answer;
 }
