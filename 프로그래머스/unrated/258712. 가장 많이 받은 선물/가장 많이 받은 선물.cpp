b25d8beb03e343256e8bc9862045d7706cc629c0
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-01-09 23:07:14 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 3.05 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -9,7 +9,9 @@ int solution(vector<string> friends, vector<string> gifts) {
     for(auto i:gifts){
         istringstream iss(i);
         iss>>give>>get;
+        //ma[A][B] : A가 B에게 준 선물 수 - A가 B에게 받은 선물 수
         ma[give][get]++,ma[get][give]--;
+        //gift[A] : A의 선물 지수 
         gift[give]++,gift[get]--;
     }
     
