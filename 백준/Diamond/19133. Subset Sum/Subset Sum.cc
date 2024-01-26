fc9aa2b6e2371f0316607731c5a28196a4649f68
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-01-27 01:55:06 +0900

    [Diamond V] Title: Subset Sum, Time: 152 ms, Memory: 10236 KB -BaekjoonHub

--- 백준/Diamond/19133. Subset Sum/Subset Sum.cc
+++ 백준/Diamond/19133. Subset Sum/Subset Sum.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<ll,ll> pll;
+typedef pair<ll,int> pli;
 int main() {
     int n,k;scanf("%d%d",&n,&k);
-    ll base=0,zero=0;
-    vector<ll> v,ans;
+    ll base=0,empty=0;
+    vector<ll> v;
     while(n--){
         ll x;scanf("%lld",&x);
         if(x<0)base+=x,v.push_back(-x);
         else v.push_back(x);
     }
     sort(v.begin(),v.end());
-    priority_queue<pll,vector<pll>,greater<pll>>pq;
-    if(base)ans.push_back(base);
+    priority_queue<pli,vector<pli>,greater<pli>>pq;
+    if(!base)empty=1;
+    else printf("%lld\n",base),k--;
     pq.push({v[0],0});
-    while(ans.size()<k){
+    while(1){
         auto[sum,l]=pq.top();pq.pop();
-        ans.push_back(sum+base);
-        if(base&&!(sum+base)&&!zero)ans.pop_back(),zero=1;
-        if(l+1==v.size())continue;
-        pq.push({sum-v[l]+v[l+1],l+1});
-        pq.push({sum+v[l+1],l+1});
+        if(l+1<v.size())pq.push({sum-v[l]+v[l+1],l+1}),pq.push({sum+v[l+1],l+1});
+        if(sum+base==0&&!empty){empty=1;continue;}
+        printf("%lld\n",sum+base),k--;
+        if(!k)break;
     }
-    for(auto i:ans)printf("%lld\n",i);
 }
