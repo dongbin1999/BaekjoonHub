b51098e49315b32e43c812cdf3706ac7a3c4b591
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-02-02 00:41:28 +0900

    [Diamond IV] Title: 백업, Time: 48 ms, Memory: 9412 KB -BaekjoonHub

---
+++ 백준/Diamond/1150. 백업/백업.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pll;
+
+int u[100001];
+pll lr[100001];
+ll c[100001];
+int find(int x){
+    if(u[x]<0)return x;
+    return u[x]=find(u[x]);
+}
+bool merge(int a,int b){
+    a=find(a),b=find(b);
+    if(a==b)return false;
+    lr[a]={min(lr[a].first,lr[b].first),max(lr[a].second,lr[b].second)};
+    u[a]+=u[b],c[a]+=c[b],u[b]=a;
+    return true;
+}
+
+void init(int n){
+    memset(u,-1,sizeof(u));
+    for(int i=1;i<n;i++)lr[i]={i,i};
+}
+
+int main() {
+    int n,k;scanf("%d%d",&n,&k);
+    init(n);
+    vector<ll> v(n),diff(n+1,-2e18);
+    priority_queue<pll,vector<pll>,greater<pll>> pq;
+    for(int i=0;i<n;i++){
+        scanf("%lld",&v[i]);
+        if(i)diff[i]=v[i]-v[i-1],pq.push({diff[i],i}),c[i]=diff[i];
+    }
+    ll ans=0;
+    while(!pq.empty()){
+        auto [cost,idx]=pq.top();pq.pop();
+        idx=find(idx);
+        auto [l,r]=lr[idx];
+        if(c[idx]!=cost||l<1||r>=n)continue;
+        //printf("cost:%lld l:%lld r:%lld\n",cost,lr[idx].first,lr[idx].second);
+        ans+=cost,k--;if(!k)break;
+        merge(idx,l-1),merge(idx,r+1);
+        c[idx]-=cost*2;
+        pq.push({c[idx],idx});
+    }
+    printf("%lld",ans);
+}
