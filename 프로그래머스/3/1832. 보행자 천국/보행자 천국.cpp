6a73a2251f55eff6a33eeb5474093f5cf5ff5d6f
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-03-21 21:55:43 +0900

    [level 3] Title: 보행자 천국, Time: 29.89 ms, Memory: 7.32 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -1,18 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int MOD = 20170805;
-int dp[2][501][501];
-
+int MOD=20170805,dp[2][501][501];
 int solution(int m, int n, vector<vector<int>> city_map) {
-    memset(dp,0,sizeof(dp));
-    dp[0][0][0]=1;
+    memset(dp,0,sizeof(dp));dp[1][0][0]=1;
     for(int i=0;i<m;i++)for(int j=0;j<n;j++){
-        if(i+1<m&&city_map[i][j]!=1)dp[0][i+1][j]=(dp[0][i+1][j]+dp[0][i][j])%MOD;
-        if(j+1<n&&city_map[i][j]!=1)dp[1][i][j+1]=(dp[1][i][j+1]+dp[1][i][j])%MOD;
-        if(i+1<m&&city_map[i][j]==0)dp[0][i+1][j]=(dp[0][i+1][j]+dp[1][i][j])%MOD;
-        if(j+1<n&&city_map[i][j]==0)dp[1][i][j+1]=(dp[1][i][j+1]+dp[0][i][j])%MOD;
+        if(city_map[i][j]!=1)dp[0][i+1][j]=(dp[0][i+1][j]+dp[0][i][j])%MOD;
+        if(city_map[i][j]!=1)dp[1][i][j+1]=(dp[1][i][j+1]+dp[1][i][j])%MOD;
+        if(city_map[i][j]==0)dp[0][i+1][j]=(dp[0][i+1][j]+dp[1][i][j])%MOD;
+        if(city_map[i][j]==0)dp[1][i][j+1]=(dp[1][i][j+1]+dp[0][i][j])%MOD;
     }
-    
     return (dp[0][m-1][n-1]+dp[1][m-1][n-1])%MOD;
 }
