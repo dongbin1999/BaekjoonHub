74dfc075b0b30d20f1007c57cc2e07fa36c215b0
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-05-02 11:07:07 +0900

    [Platinum III] Title: 뜨끈한 돼지국밥, Time: 48 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Platinum/17948. 뜨끈한 돼지국밥/뜨끈한 돼지국밥.cc
+++ 백준/Platinum/17948. 뜨끈한 돼지국밥/뜨끈한 돼지국밥.cc
@@ -3,22 +3,20 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> pll;
 pll dp[5001];
-ll sum[5002],cost[5001][5001];
+ll sum[5002],v[5001];
+ll cost(int l,int r){
+    int mid=l+r>>1;
+    return sum[r]-sum[mid]-v[mid]*(r-mid)+v[mid]*(mid-l+1)-(sum[mid]-sum[l-1]);
+}
 int main(){
     int n;scanf("%d",&n);
-    vector<ll> v(n+1);
     for(int i=1;i<=n;i++)scanf("%lld",&v[i]);
     ll c,m;scanf("%lld%lld",&c,&m);
-    sort(v.begin(),v.end());
+    sort(v+1,v+1+n);
     for(int i=1;i<=n;i++)sum[i]=sum[i-1]+v[i];
-    for(int l=1;l<=n;l++)for(int r=l;r<=n;r++){
-        int mid=l+r>>1;
-        cost[l][r]=sum[r]-sum[mid]-v[mid]*(r-mid);
-        cost[l][r]+=v[mid]*(mid-l+1)-(sum[mid]-sum[l-1]);
-    }
     memset(dp,0x3f,sizeof(dp));
     dp[0]={0,0};
     for(int i=1;i<=n;i++)for(int j=1;j<=i;j++)
-        dp[i]=min(dp[i],{dp[i-j].first+cost[i-j+1][i]*c+m,dp[i-j].second+1});
+        dp[i]=min(dp[i],{dp[i-j].first+cost(i-j+1,i)*c+m,dp[i-j].second+1});
     printf("%lld %lld",dp[n].first,dp[n].second);
 }
