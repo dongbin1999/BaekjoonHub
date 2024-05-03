77676af0dc424f238176ddbf3d221029b18c87f4
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-05-03 19:39:30 +0900

    [Platinum III] Title: 검은점과 하얀점 연결, Time: 92 ms, Memory: 6088 KB -BaekjoonHub

--- 백준/Platinum/2647. 검은점과 하얀점 연결/검은점과 하얀점 연결.cc
+++ 백준/Platinum/2647. 검은점과 하얀점 연결/검은점과 하얀점 연결.cc
@@ -24,11 +24,10 @@ int main(){
     mx=n/2;
     for(int i=1;i<=n;i++)scanf("%1d",&a[i]);
     memset(dp,0x3f,sizeof(dp));
-    for(int i=1;i<n;i++)if(a[i]^a[i+1])
-        for(int H=1;H<=mx;H++)dp[i][i+1][H]={3,i+1};
+    for(int i=1;i<n;i++)if(a[i]^a[i+1])dp[i][i+1][1]={3,i+1};
     for(int len=2;len<=n;len+=2)for(int l=1,r=l+len-1;r<=n;l++,r++){
         for(int h=1;h<=mx;h++){
-            for(int H=h;H<=mx;H++)dp[l][r][H]=min(dp[l][r][H],dp[l][r][h]);
+            dp[l][r][h]=min(dp[l][r][h],dp[l][r][h-1]);
             for(int m=l+1;m+1<=r-1;m+=2){
                 auto [lc,lr]=dp[l][m][h];
                 auto [rc,rr]=dp[m+1][r][h];
