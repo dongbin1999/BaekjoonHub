7259d3bf59ef8f52f09e0ca032884d92c712fb4b
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-05-06 13:37:08 +0900

    [Platinum III] Title: 대문자, Time: 16 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Platinum/13906. 대문자/대문자.cc
+++ 백준/Platinum/13906. 대문자/대문자.cc
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int,int> pii;
 const ll mod=1e9+7;
 
 char s[1002];
@@ -12,14 +11,15 @@ int idx(int cur,int al){
     if(l+2>=b[al].size())return -1;
     return b[al][l+2];
 }
-ll solve(){
-    for(int i=0;i<26;i++)b[i].clear();
+
+int main(){
+    scanf("%s",s+1);
     int n=strlen(s+1);
     for(int i=1;i<=n;i++)b[s[i]-'a'].push_back(i);
     vector<ll> dp(n+1,0),init(n+1);
     dp[0]=1;
     ll ans=0;
-    for(int len=0;len<=n;len++){
+    for(int len=0;len*3<n;len++){
         vector<ll> tmp=init;
         for(int nx_al=0;nx_al<26;nx_al++)for(int cur=0;cur<=n;cur++){
                 if(!dp[cur])continue;
@@ -30,16 +30,6 @@ ll solve(){
         dp=tmp;
         for(int cur=0;cur<=n;cur++)ans=(ans+dp[cur])%mod;
     }
-    return ans;
-}
-
-int main(){
-    scanf("%s",s+1);
-    int n=strlen(s+1);
-    ll x=solve();
-    for(int l=1,r=n;l<=r;l++,r--)swap(s[l],s[r]);
-    ll y=solve();
-    assert(x==y);
-    printf("%lld",x);
+    printf("%lld",ans);
     return 0;
 }
