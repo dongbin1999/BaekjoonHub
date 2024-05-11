aad2411c46152fdb19e8c4d634290d09a6c2b4b3
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-05-11 23:59:14 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef array<int,3> tup;
+const ll mod=998244353;
+#define UNIQUE(v) sort(v.begin(),v.end()),v.erase(unique(v.begin(),v.end()),v.end());
+ll GCD(ll a,ll b){return b?GCD(b,a%b):a;}
+ll power(ll a,ll b){ll ret=1;while(b){if(b%2)ret=ret*a%mod;a=a*a%mod;b/=2;}return ret;}
+
+
+int main(){
+    int a,b,c;scanf("%d%d%d",&a,&b,&c);
+    if(a+b==c)printf("correct!");
+    else printf("wrong!");
+    return 0;
+}
