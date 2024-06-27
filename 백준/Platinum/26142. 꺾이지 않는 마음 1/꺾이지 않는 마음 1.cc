96efc2ab61ddaf5b705952a9e97f6293eedeb6e7
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-06-27 15:39:21 +0900

    [Platinum V] Title: 꺾이지 않는 마음 1, Time: 236 ms, Memory: 2344 KB -BaekjoonHub

--- 백준/Platinum/26142. 꺾이지 않는 마음 1/꺾이지 않는 마음 1.cc
+++ 백준/Platinum/26142. 꺾이지 않는 마음 1/꺾이지 않는 마음 1.cc
@@ -2,20 +2,22 @@
 using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> pll;
-ll dp[10001][10001],ans[10001];
 
 int main(){
     int n;scanf("%d",&n);
     vector<pll> v(n+1);
     for(int i=1;i<=n;i++)scanf("%lld%lld",&v[i].first,&v[i].second);
     sort(v.begin(),v.end());
+    vector<ll> dp(10001),ans(10001);
     for(int i=1;i<=n;i++){
+        auto nx=dp;
         for(int x=0;x<i;x++){
-            dp[i][x]=max(dp[i][x],dp[i-1][x]);
-            dp[i][x+1]=max(dp[i][x+1],dp[i-1][x]+v[i].first*x+v[i].second);
+            nx[x]=max(nx[x],dp[x]);
+            nx[x+1]=max(nx[x+1],dp[x]+v[i].first*x+v[i].second);
         }
+        dp=nx;
+        for(int j=1;j<=n;j++)ans[j]=max(ans[j],dp[j]);
     }
-    for(int i=0;i<=n;i++)for(int j=0;j<=n;j++)ans[j]=max(ans[j],dp[i][j]);
     for(int i=1;i<=n;i++)printf("%lld\n",ans[i]);
     return 0;
 }
