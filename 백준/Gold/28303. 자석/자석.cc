f09784c507962ff103123bf8196a8158d70cdba0
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-07-06 22:45:45 +0900

    [Gold III] Title: 자석, Time: 120 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/28303. 자석/자석.cc
+++ 백준/Gold/28303. 자석/자석.cc
@@ -1,24 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<ll,ll> pll;
 
-pll mn[500002],r[500002];
-ll a[500002];
+ll a[500001];
 
 int main(){
     int n;ll k;scanf("%d%lld",&n,&k);
     for(int i=1;i<=n;i++)scanf("%lld",&a[i]);
-    memset(mn,0x3f,sizeof(mn));memset(r,0x3f,sizeof(r));
-    for(int i=1;i<=n;i++)
-        mn[i]=min(mn[i-1],{a[i]-k*i,i});
-    for(int i=n;i>=1;i--)
-        r[i]=min(r[i+1],{a[i]+k*i,i});
-    ll ans=-2e18;
-    for(int i=1;i<=n;i++){
-        ll x=mn[i-1].second,y=r[i+1].second;
-        if(x<1e9)ans=max(ans,a[i]-abs(x-i)*k-a[x]);
-        if(y<1e9)ans=max(ans,a[i]-abs(y-i)*k-a[y]);
+    ll x=0,ans=-2e18;
+    for(int i=1;i<n;i++){
+        ll cur=a[i]-a[i+1]-k;
+        x=max(x,0ll);
+        x+=cur;
+        ans=max(ans,x);
+    }
+    x=0;
+    for(int i=n;i>1;i--){
+        ll cur=a[i]-a[i-1]-k;
+        x=max(x,0ll);
+        x+=cur;
+        ans=max(ans,x);
     }
     printf("%lld",ans);
+    return 0;
 }
