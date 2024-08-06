6e70ba9595a585ae4b3d81ec0d449d526bcfb9ea
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-08-06 15:47:48 +0900

    [Platinum I] Title: 배수, Time: 388 ms, Memory: 4612 KB -BaekjoonHub

---
+++ 백준/Platinum/2595. 배수/배수.cc
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,string> pis;
+#define over(s) (s.size()>5||stoi(s)>n)
+#define UNIQUE(v) sort(v.begin(),v.end()),v.erase(unique(v.begin(),v.end()),v.end());
+int n;
+set<vector<int>> se;
+
+bool operator<(pis a,pis b){
+    if(a.first==b.first)
+        return a.second.size()==b.second.size()?a>b:a.second.size()>b.second.size();
+    else return a.first>b.first;
+}
+
+string b[2][30000];
+pis f(vector<int> v){
+    for(int i=0;i<n;i++)b[0][i]=b[1][i]="";
+    queue<pis> q;
+    for(auto i:v){
+        if(!i)continue;
+        string s=to_string(i);
+        int x=over(s),y=i%n;
+        if(b[x][y]!="")continue;
+        q.push({y,s}),b[x][y]=1;
+    }
+    while(!q.empty()){
+        auto [r,s]=q.front();q.pop();
+        if(!r)return {v.size(),s};
+        for(auto ni:v){
+            string ns=s+to_string(ni);
+            int nx=over(ns),ny=(r*10+ni)%n;
+            if(b[nx][ny]!="")continue;
+            q.push({ny,ns}),b[nx][ny]=1;
+        }
+    }
+    return {9,""};
+}
+
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<100000;i++){
+        vector<int> v;int x=i;
+        while(x)v.push_back(x%10),x/=10;
+        UNIQUE(v);
+        se.insert(v);
+    }
+    pis ans={9,""};
+    for(auto v:se){
+        if(ans.first<v.size())continue;
+        pis x=f(v);
+        if(ans<x)ans=x;
+    }
+    cout<<ans.second;
+    return 0;
+}
