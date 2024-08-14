c490bca600f4196ce0ee9ab496d963a41392122f
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-08-14 19:39:42 +0900

    [Silver V] Title: 팔찌 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;scanf("%d",&n);
+    vector<int> v(n);for(int i=0;i<n;i++)scanf("%d",&v[i]);
+    sort(v.begin(),v.end());
+    printf("%d",v.back()-v[0]<<1);
+    return 0;
+}
