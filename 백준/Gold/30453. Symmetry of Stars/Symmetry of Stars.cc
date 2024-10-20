5132472df243999bf2c48370840d4ec5b7174d6f
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-10-20 11:08:12 +0900

    [Gold IV] Title: Symmetry of Stars, Time: 824 ms, Memory: 41756 KB -BaekjoonHub

---
+++ 백준/Gold/30453. Symmetry of Stars/Symmetry of Stars.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+int main(){
+    int n;scanf("%d",&n);
+    unordered_map<ll,int> ma;
+    vector<pii> v(n);
+    for(auto &[x,y]:v)scanf("%d%d",&x,&y);
+    for(auto [x,y]:v)for(auto [x2,y2]:v)
+        ma[(ll)(x+x2)*1e9+y+y2]++;
+    int ans=0;
+    for(auto [a,b]:ma)ans=max(ans,b);
+    printf("%d",ans);
+    return 0;
+}
