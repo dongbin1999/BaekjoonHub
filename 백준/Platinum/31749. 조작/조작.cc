4d4f4d24549856818f31d9b1e83fc635d413bc9a
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-12-11 17:02:15 +0900

    [Platinum V] Title: 조작, Time: 76 ms, Memory: 8964 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/31749. 조작/조작.cc
+++ 백준/Platinum/31749. 조작/조작.cc
@@ -3,24 +3,24 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> pll;
 int main(){
-    int n;scanf("%d",&n);
+    ll n;scanf("%lld",&n);
     vector<ll> v(n);
     for(auto &x:v)scanf("%lld",&x);
     vector<pll> ans;
-    for(int i=0;i<n-1;i++){
+    for(ll i=0;i<n-1;i++){
         ll d=-v[i];
         ans.push_back({i,d}),v[i]+=d,v[i+1]+=d;
     }
     if(n%2){
-        for(int i=0;i<n-1;i+=2)
+        for(ll i=0;i<n-1;i+=2)
             ans.push_back({i,v.back()}),v[i]+=v.back(),v[i+1]+=v.back();
     }
     else{
         ll x=(abs(v.back())+n/2-1)/(n/2);
         if(v.back()<0)x*=-1;
-        for(int i=n-2;i>=0;i--)
+        for(ll i=n-2;i>=0;i--)
             if(i%2==0){
-                ll d=x-v[i+1];
+                ll d=x>0?min(0ll,x-v[i+1]):max(0ll,x-v[i+1]);
                 ans.push_back({i,d}),v[i]+=d,v[i+1]+=d;
             }
             else{
