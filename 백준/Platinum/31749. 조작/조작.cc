43286f49b837e2743e90df8c25fd176dd2b23d46
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2024-12-11 16:50:24 +0900

    [Platinum V] Title: 조작, Time: 60 ms, Memory: 8964 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Platinum/31749. 조작/조작.cc
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pll;
+int main(){
+    int n;scanf("%d",&n);
+    vector<ll> v(n);
+    for(auto &x:v)scanf("%lld",&x);
+    vector<pll> ans;
+    for(int i=0;i<n-1;i++){
+        ll d=-v[i];
+        ans.push_back({i,d}),v[i]+=d,v[i+1]+=d;
+    }
+    if(n%2){
+        for(int i=0;i<n-1;i+=2)
+            ans.push_back({i,v.back()}),v[i]+=v.back(),v[i+1]+=v.back();
+    }
+    else{
+        ll x=(abs(v.back())+n/2-1)/(n/2);
+        if(v.back()<0)x*=-1;
+        for(int i=n-2;i>=0;i--)
+            if(i%2==0){
+                ll d=x-v[i+1];
+                ans.push_back({i,d}),v[i]+=d,v[i+1]+=d;
+            }
+            else{
+                ll d=-v[i+1];
+                ans.push_back({i,d}),v[i]+=d,v[i+1]+=d;
+            }
+    }
+    printf("%lld\n%d\n",*max_element(v.begin(),v.end())-*min_element(v.begin(),v.end()),ans.size());
+    for(auto [a,b]:ans)printf("%lld %lld\n",a+1,b);
+}
