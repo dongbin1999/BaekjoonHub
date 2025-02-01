68225f6c7136528491cb0b4565fb0a5040aca864
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2025-02-01 14:29:01 +0900

    [Silver V] Title: Minimal Coprime, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/33310. Minimal Coprime/Minimal Coprime.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef array<int,3> tup;
+const ll mod=1e9+7;
+#define UNIQUE(v) sort(v.begin(),v.end()),v.erase(unique(v.begin(),v.end()),v.end());
+ll GCD(ll a,ll b){return b?GCD(b,a%b):a;}
+ll power(ll a,ll b){ll ret=1;while(b){if(b%2)ret=ret*a%mod;a=a*a%mod;b/=2;}return ret;}
+
+void solve(){
+    int l,r;scanf("%d%d",&l,&r);
+    printf("%d\n",r-l+(r==1));
+}
+
+int main(){
+    int tc;scanf("%d",&tc);
+    while(tc--)solve();
+    return 0;
+}
