fed9fa1cd91eee07041906c88daddcd4a0f5dc78
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2025-03-30 21:44:04 +0900

    [Gold III] Title: 하늘에서 떨어지는 $\infty$개의 별, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/33682. 하늘에서 떨어지는 $＼infty$개의 별/하늘에서 떨어지는 $＼infty$개의 별.cc
+++ 백준/Gold/33682. 하늘에서 떨어지는 $＼infty$개의 별/하늘에서 떨어지는 $＼infty$개의 별.cc
@@ -1,14 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
 int main(){
-    ll s,x,m,d,k;scanf("%lld%lld%lld%lld%lld",&s,&x,&m,&d,&k);
-    ll g=__gcd(x,m);
-    s%=g;ll mx=m-g+s;
+    long long s,x,m,d,k,g,ok,mx;cin>>s>>x>>m>>d>>k;
+    g=__gcd(x,m);
+    s%=g,mx=m-g+s;
     if(!mx)return !printf("0");
-    ll ok=k/mx;
-    if(!ok)printf("0");
-    else printf("%lld",(d-1)/ok);
-    return 0;
+    ok=k/mx,cout<<(ok?(d-1)/ok:0);
 }
