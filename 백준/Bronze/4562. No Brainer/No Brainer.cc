b675e3fcc371a9ba96a209f5fc1f92aa038728ff
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2025-08-26 22:18:19 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;scanf("%d",&t);
+    while(t--){
+        int x,y;scanf("%d%d",&x,&y);
+        printf(x<y?"NO":"MMM");
+        printf(" BRAINS\n");
+    }
+}
