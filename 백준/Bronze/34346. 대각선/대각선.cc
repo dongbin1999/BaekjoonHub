ce5e77788e09c180c435cdd2e705cb6ed2c4654d
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2025-11-11 22:15:51 +0900

    [Bronze IV] Title: 대각선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34346. 대각선/대각선.cc
@@ -0,0 +1,24 @@
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef array<int,3> arr;
+const ll mod=1e9+7;
+#define UNIQUE(v) sort(v.begin(),v.end()),v.erase(unique(v.begin(),v.end()),v.end());
+ll GCD(ll a,ll b){return b?GCD(b,a%b):a;}
+ll power(ll a,ll b){ll ret=1;while(b){if(b%2)ret=ret*a%mod;a=a*a%mod;b/=2;}return ret;}
+
+void solve(){
+    int n;cin>>n;
+    cout<<(1+!(n%2));
+}
+
+int main(){
+    int tc=1;
+    while(tc--)solve();
+    return 0;
+}
