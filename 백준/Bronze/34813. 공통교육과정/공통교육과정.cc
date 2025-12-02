65387f8968675706904d2e4372dfb7e12e91e95f
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2025-12-02 22:33:03 +0900

    [Bronze V] Title: 공통교육과정, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.cc
@@ -0,0 +1,8 @@
+#include<cstdio>
+int main(){
+    char c;scanf("%c",&c);
+    if(c=='F')printf("Foundation");
+    if(c=='C')printf("Claves");
+    if(c=='V')printf("Veritas");
+    if(c=='E')printf("Exploration");
+}
