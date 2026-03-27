3693d3df1e8f0ec347c8c5e550f6552a190046cc
이동빈 <62780290+dongbin1999@users.noreply.github.com>
2026-03-27 21:03:25 +0900

    [Platinum V] Title: 허술한 보안 프로그램, Time: 4 ms, Memory: 13832 KB -BaekjoonHub

--- 백준/Platinum/32169. 허술한 보안 프로그램/허술한 보안 프로그램.cc
+++ 백준/Platinum/32169. 허술한 보안 프로그램/허술한 보안 프로그램.cc
@@ -21,9 +21,7 @@ int main(){
         a[i]=i,b[i]=x^i;
         while(b[i]>=n||se.count(b[i]))b[i]^=1<<(31-__builtin_clz(b[i]));
         se.insert(b[i]);
-        assert(b[i]>=0);
     }
-    for(int i=0;i<n;i++)assert((a[i]&b[i])==0);
     a=query(a),b=query(b);
     printf("! ");
     for(int i=0;i<n;i++)printf("%d ",a[i]&b[i]);
